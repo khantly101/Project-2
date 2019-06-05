019d4146761e1b1a279fb20e80ddb34e212d93fe
khantly101 <khantuanly@gmail.com>
2019-06-04 20:24:45 -0700

    removed logs

--- controllers/user.js
+++ controllers/user.js
@@ -86,31 +86,25 @@ router.post('/login', (req, res) => {
 
 	usernameInUse = false
 	missingText = false
-	console.log("point1")
+
 	if (req.body.password) {
-		console.log("point2")
 		User.findOne({ username: req.body.username }, (err, foundUser) => {
-			console.log("point3")
 			if (foundUser) {
 				console.log(foundUser)
 				if ( bcrypt.compareSync(req.body.password, foundUser.password)) {
 					req.session.currentUser = foundUser
 					wrongpass = false
-					console.log("point4")
 					res.redirect('../main')
 				} else {
-					console.log("point5")
 					wrongpass = true
 					res.redirect('/')
 				}
 			} else {
-				console.log("point6")
 				wrongpass = true
 				res.redirect('/')
 			}
 		})
 	} else {
-		console.log("point7")
 		wrongpass = true
 		res.redirect('/')
 	}

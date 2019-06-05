607d9155a9af32ce722d082874fea351ca09cce1
khantly101 <khantuanly@gmail.com>
2019-06-04 19:50:52 -0700

    test

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const controllerUser	= require('./controllers/user.js')
 
 const app 				= express()
 const port 				= process.env.PORT || 3000
-const mongoURI			= process.env.MONGODB_URI || 'mongodb://localhost:27017/project-2'
+const MONGODB_URI		= process.env.MONGODB_URI || 'mongodb://localhost:27017/project-2'
 
 //////////////////////////////
 // MONGOOSE

7a115ac2aebf661782c0d986fc421b83dfdbbdaf
khantly101 <khantuanly@gmail.com>
2019-06-06 23:16:11 -0700

    more styling for index

--- public/app.js
+++ public/app.js
@@ -36,19 +36,37 @@ const pieChart = (chartData) => {
 
 	let arc = d3.arc().outerRadius(radius - 20).innerRadius(90)
 
-	let svg = d3.select("#pie").append("svg").attr("width", width).attr("height", height).append("g").attr("transform", "translate(" + width/2 + "," + height/2 +")")	
-	let g = svg.selectAll("arc").data(pie).enter().append("g").attr("class", "arc")
+	let svg = d3.select("#pie")
+				.append("svg")
+				.attr("width", width)
+				.attr("height", height)
+				.append("g")
+				.attr("transform", "translate(" + width/2 + "," + height/2 +")")	
+
+	let g = svg.selectAll("arc")
+				.data(pie)
+				.enter()
+				.append("g")
+				.attr("class", "arc")
 
 	svg.append("svg:circle")
-   		.attr("cx", 0)
-    	.attr("cy", 0)
-    	.attr("r", radius /2)
-    	.attr("class", "middle")
-    	.style("fill", "grey")
+		.attr("cx", 0)
+		.attr("cy", 0)
+		.attr("r", radius /2)
+		.attr("class", "middle")
+		.style("fill", "grey")
 
-	g.append("path").attr("d", arc).style("fill", (d) => {
-		return color(d.data.Type)
-	})
+	svg.append("text")
+		.attr("text-anchor", "middle")
+		.attr("class", "Type")
+		.text("Fuel Type")
+		.attr('y', height * -0.1)
+
+	g.append("path")
+		.attr("d", arc)
+		.style("fill", (d) => {
+			return color(d.data.Type)
+		})
 
 	
 
@@ -73,19 +91,37 @@ const brandChart = (chartData) => {
 
 	let arc = d3.arc().outerRadius(radius - 20).innerRadius(90)
 
-	let svg = d3.select("#brand").append("svg").attr("width", width).attr("height", height).append("g").attr("transform", "translate(" + width/2 + "," + height/2 +")")	
-	let g = svg.selectAll("arc").data(pie).enter().append("g").attr("class", "arc")
+	let svg = d3.select("#brand")
+				.append("svg")
+				.attr("width", width)
+				.attr("height", height)
+				.append("g")
+				.attr("transform", "translate(" + width/2 + "," + height/2 +")")	
+
+	let g = svg.selectAll("arc")
+				.data(pie)
+				.enter()
+				.append("g")
+				.attr("class", "arc")
 
 	svg.append("svg:circle")
-   		.attr("cx", 0)
-    	.attr("cy", 0)
-    	.attr("r", radius /2)
-    	.attr("class", "middle")
-    	.style("fill", "grey")
+		.attr("cx", 0)
+		.attr("cy", 0)
+		.attr("r", radius /2)
+		.attr("class", "middle")
+		.style("fill", "grey")
 
-	g.append("path").attr("d", arc).style("fill", (d) => {
-		return color(d.data.Brand)
-	})
+	svg.append("text")
+		.attr("text-anchor", "middle")
+		.attr("class", "Type")
+		.text("Brand")
+		.attr('y', height * -0.1)
+
+	g.append("path")
+		.attr("d", arc)
+		.style("fill", (d) => {
+			return color(d.data.Brand)
+		})
 
 	
 
@@ -102,28 +138,42 @@ const lineChart = (chartData) => {
 	let width = 600
 	let height = 300
 
-	let vis = d3.select("#line").append("svg").attr("width", width).attr("height", height)
+	let vis = d3.select("#line")
+				.append("svg")
+				.attr("width", width)
+				.attr("height", height)
 
 	let xScale = d3.scaleLinear().domain([0, 10]).range([0, 400])
 	let xAxis = d3.axisBottom().scale(xScale)
-	let x = vis.append("g").attr("class", "x axis").attr("transform", "translate(50,210)").call(xAxis)
+	let x = vis.append("g").attr("class", "x axis").attr("transform", "translate(25,210)").call(xAxis)
 
 	let yScale = d3.scaleLinear().domain([40, 0]).range([0, 200])
 	let yAxis = d3.axisLeft().scale(yScale)
-	let y = vis.append("g").attr("class", "y axis").attr("transform", "translate(50,10)").call(yAxis)
+	let y = vis.append("g").attr("class", "y axis").attr("transform", "translate(25,10)").call(yAxis)
 
 	let line = d3.line().x((d,i) => {
-		return xScale(i) + 50
+		return xScale(i) + 25
 	}).y((d) => {
 		return yScale(d.y) + 10
 	}).curve(d3.curveMonotoneX)
 
-	vis.append("path").datum(chartData).attr("class", "line").attr("d", line)
-	vis.selectAll(".dot").data(chartData).enter().append("circle").attr("class", "dot").attr("cx", (d, i) => {
-		return xScale(i) + 50
-	}).attr("cy", (d) => {
-		return yScale(d.y) + 10
-	}).attr("r", 4)
+	vis.append("path")
+		.datum(chartData)
+		.attr("class", "line")
+		.attr("d", line)
+
+	vis.selectAll(".dot")
+		.data(chartData)
+		.enter()
+		.append("circle")
+		.attr("class", "dot")
+		.attr("cx", (d, i) => {
+			return xScale(i) + 25
+		})
+		.attr("cy", (d) => {
+			return yScale(d.y) + 10
+		})
+		.attr("r", 4)
 
 }
 
